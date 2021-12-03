0bfb89ff477ea8ef31374f560dcabe7612ad4cfc
Slymaster <sylvain.rey.pro@gmail.com>
2021-12-03 12:36:19 +0100

    add router dependencies and upgrade to 0.8.10

--- Uniswap/contracts/SRSwapRouter02.sol
+++ Uniswap/contracts/SRSwapRouter02.sol
@@ -1,7 +1,7 @@
-pragma solidity =0.6.6;
+pragma solidity 0.8.10;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
-import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
+import './interfaces/IUniswapV2Factory.sol';
+import './libraries/TransferHelper.sol';
 
 import './interfaces/IUniswapV2Router02.sol';
 import './libraries/UniswapV2Library.sol';
