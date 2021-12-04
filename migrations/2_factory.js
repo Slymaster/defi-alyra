717ee6eeef3a5cb169ce492d1da9d4704cb151e0
Slymaster <sylvain.rey.pro@gmail.com>
2021-12-04 15:57:04 +0100

    add migrations files

--- migrations/2_factory.js
+++ migrations/2_factory.js
@@ -1,5 +1,44 @@
-const Factory = artifacts.require("Uniswap/SRSwapFactory");
+const Factory = artifacts.require("Uniswap/SBSwapFactory.sol");
+const FakeUSDC = artifacts.require("Uniswap/FakeUSDC.sol");
+const FakeUSDT = artifacts.require("Uniswap/FakeUSDT.sol");
+const FakeDAI = artifacts.require("Uniswap/FakeDai.sol");
+const WETH = artifacts.require("Uniswap/WETH.sol");
 
-module.exports = function (deployer) {
-    deployer.deploy(Factory);
+module.exports = async function (deployer, network, addresses) {
+    //|:::::: Deploy the Factory :::::::|
+    await deployer.deploy(Factory, addresses[0]); // constructor(address _feeToSetter)
+    //address that receives the payment for the trading fees
+    //with truffle, by default the first address is use for the deployment
+    const factory = await Factory.deployed();
+    await Factory.deployed();
+    //|:::::::::::::::::::::::::::::::::|
+
+    let fakeUSDCAddress, fakeUSDTAddress, fakeDAIAddress, wethAddress;
+    if(network === 'mainnet') {
+        fakeUSDCAddress = '';
+        fakeUSDTAddress = '';
+        fakeDAIAddress = '';
+        wethAddress = '';
+    } else {
+        await deployer.deploy(FakeUSDC) //deploy
+        await deployer.deploy(FakeUSDT)
+        await deployer.deploy(FakeDAI)
+        await deployer.deploy(WETH)
+        // wait mining finishes and get a reference to the token
+        const fakeUSDC = await FakeUSDC.deployed();
+        const fakeUSDT = await FakeUSDT.deployed();
+        const fakeDAI = await FakeDAI.deployed();
+        const weth = await WETH.deployed();
+        // get the tokens' address
+        fakeUSDCAddress = fakeUSDC.address;
+        fakeUSDTAddress = fakeUSDT.address;
+        fakeDAIAddress = fakeDAI.address;
+        wethAddress = weth.address;
+
+    }
+    // create pairs
+    await factory.createPair(fakeUSDCAddress, wethAddress);
+    await factory.createPair(fakeUSDTAddress, wethAddress);
+    await factory.createPair(fakeDAIAddress, wethAddress);
+
 };
--- migrations/3_pair.js
+++
@@ -1,5 +0,0 @@
-const Pair = artifacts.require("Uniswap/SRSwapPair");
-
-module.exports = function (deployer) {
-    deployer.deploy(Pair);
-};
